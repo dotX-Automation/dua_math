01e866290b1bca678797ffe84f9ab53dd59cf44b
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2026-02-12 16:22:52 +0100

    fix: typo

--- include/dua_math/dua_math.hpp
+++ include/dua_math/dua_math.hpp
@@ -88,8 +88,8 @@ template<typename TypeT, std::enable_if_t<std::is_floating_point<TypeT>::value, 
   dead = std::abs(dead);
   lo = std::abs(lo);
   hi = std::abs(hi);
-  if (hi < lo) lo = hi;
-  if (lo < dead) lo = dead;
+  if (hi < lo) {lo = hi;}
+  if (lo < dead) {lo = dead;}
   const TypeT abs_value = std::abs(value);
   if (abs_value <= dead) {
     return static_cast<TypeT>(0);
